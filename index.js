cc7671969eb4e55745cf910ff2cd999750cafcc9
Vu Phong Nguyen <vuphongnguyen87@gmail.com>
2018-08-08 14:06:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 // write your code below!
-var name = "Joe"
-var height = 40
+var name = "Susan"
+var height = 39
 var message = `Name is ${name} and is ${height} inches tall`
 
-module.exports = {name, height, message}
+module.exports = {
+  name,
+  height,
+  message
+}
